86010e91aa7d0083ad3bbf53e8b0d22a99b87296
Danny Lade <danny.lade@iteratec.de>
2019-04-13 11:23:30 +0200

    Using setup now.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This repo contains a NullFS based on the FUSE filesystem in Python.
 
 The code is based on this article: 
 https://www.stavros.io/posts/python-fuse-filesystem/
+
+# Install
+pip install -e .
+
+(will install [fusepy](https://pypi.org/project/fusepy/))
---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Danny Lade.
+#
+"""
+@author: Danny Lade
+"""
+from os import path
+from setuptools import setup, find_packages
+
+
+def __readme__():
+    return open(path.join(path.dirname(__file__), 'README.md')).read()
+
+
+setup(
+    name='python-nullfs',
+    version='0.8',
+    author="Danny Lade",
+    author_email="dannylade@gmail.com",
+    description=("A client for the simple tron-like multi client online game called 'Traze' which is using MQTT for communication."),  # noqa
+    license='GPLv3',
+    keywords="fuse fusepy nullfs filesystem",
+    url="https://github.com/DLade/python-nullfs",
+    long_description=__readme__(),
+    long_description_content_type='text/markdown',
+
+    packages=find_packages('.'),
+
+    install_requires=[
+        'python_version>="3.5"',
+        'fusepy==2.0.4',
+    ],
+
+    classifiers=[
+        # Development Status :: 1 - Planning
+        # Development Status :: 2 - Pre-Alpha
+        # Development Status :: 3 - Alpha
+        # Development Status :: 4 - Beta
+        # Development Status :: 5 - Production/Stable
+        # Development Status :: 6 - Mature
+        # Development Status :: 7 - Inactive
+        "Development Status :: 1 - Planning",
+        "Topic :: System :: Filesystems",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
